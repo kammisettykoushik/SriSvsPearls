08413401fbcf1b16fc27e01ce18132fd295649ea
kammisettykoushik <koushiknaidu2@gmail.com>
2024-12-30 13:05:08 +0530

    update in allowed hosts

--- svs/settings.py
+++ svs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*','srisvspearls.com', 'www.srisvspearls.com']
+ALLOWED_HOSTS = ['62.72.12.232','srisvspearls.com', 'www.srisvspearls.com']
 
 
 # settings.py
