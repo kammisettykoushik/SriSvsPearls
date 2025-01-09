ec6956d6ba9f2390a60899bc2c5e20e51507a8ff
OmPrakashMadasi <koushiknaidu2@gmail.com>
2025-01-09 12:04:35 +0530

    product model price updation

--- webapp/models.py
+++ webapp/models.py
@@ -56,7 +56,7 @@ class Category(models.Model):
 class Product(models.Model):
     objects = None
     name = models.CharField(max_length=100)
-    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
@@ -64,7 +64,7 @@ class Product(models.Model):
     is_published = models.BooleanField(default=True)
     created_at = models.DateTimeField(default=datetime.now, blank=True)
     is_sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    sale_price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
 
     def __str__(self):
         return self.name
